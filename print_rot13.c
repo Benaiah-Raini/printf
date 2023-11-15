2689f0b98c3b9a0cd080fd04f198a3e09fa8ac9d
Benaiah-Raini <benaiahraini@gmail.com>
2023-11-14 18:40:08 -0800

    rot13

--- print_rot13.c
+++ print_rot13.c
@@ -10,34 +10,33 @@
  */
 int _rot13(va_list args)
 {
-    char *original = va_arg(args, char *);
-    char *s;
-    int a, b, count;
-    char data[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char *original = va_arg(args, char *);
+char *s;
+int a, b, count;
+char data[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-    s = strdup(original);
-    if (s == NULL)
-    {
-        // Handle memory allocation failure
-        return 0;
-    }
-
-    for (a = 0; s[a] != '\0'; a++)
-    {
-        for (b = 0; b < 52; b++)
-        {
-            if (s[a] == data[b])
-            {
-                s[a] = rot[b];
-                break;
-            }
-        }
-    }
-
-    _pristr(s);
-    count = leng_to_string(s);
-    free(s);
-    return count;
+s = strdup(original);
+if (s == NULL)
+{
+/* Handle memory allocation failure*/
+return 0;
 }
 
+for (a = 0; s[a] != '\0'; a++)
+{
+for (b = 0; b < 52; b++)
+{
+if (s[a] == data[b])
+{
+s[a] = rot[b];
+break;
+}
+}
+}
+
+_pristr(s);
+count = _leng_to_string(s);
+free(s);
+return count;
+}

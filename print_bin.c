03c6ebf1e691069863b5378e72f978d2c770182b
N-benitha <ngungabn03@gmail.com>
2023-11-14 16:07:57 -0800

    binary conversion

--- print_bin.c
+++ print_bin.c
@@ -11,19 +11,19 @@ int print_bin(va_list val)
 {
 	int flag = 0;
 	int count = 0;
-	int i, a = 1, b;
+	int i, b;
 	unsigned int num = va_arg(val, unsigned int);
 	unsigned int p;
 
 	for (i = 0; i < 32; i++)
 	{
-		p = ((a << (32 - i)) & num);
+		p = ((1u << (31 - i)) & num);
 		if (p >> (31 - i))
 			flag = 1;
 		if (flag)
 		{
 			b = p >> (31 - i);
-			_prints(b + 48);
+			_prints(b + '0');
 			count++;
 		}
 	}

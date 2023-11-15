3cddc2d320635538a1306c3fdd61d689136d36d3
N-benitha <ngungabn03@gmail.com>
2023-11-14 17:30:03 -0800

    prints the string with non-printable characters

--- print_other_string.c
+++ print_other_string.c
@@ -19,21 +19,21 @@ int print_other_string(va_list val)
 	{
 		if (s[i] < 32 || s[i] >= 127)
 		{
-			_putchar('\\');
-			_putchar('x');
+			_prints('\\');
+			_prints('x');
 			len = len + 2;
 			value = s[i];
 
 			if (value < 16)
 			{
-				_putchar('0');
+				_prints('0');
 				len++;
 			}
 			len = len + print_HEX_extra(value);
 		}
 		else
 		{
-			_putchar(s[i]);
+			_prints(s[i]);
 			len++;
 		}
 	}

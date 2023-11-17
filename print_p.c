38eab26a2197ca9adb0fea1875bb50f30ab99ca0
Benaiah-Raini <benaiahraini@gmail.com>
2023-11-17 14:19:04 -0800

    p-error

--- print_p.c
+++ print_p.c
@@ -1,11 +1,11 @@
 #include "main.h"
+
 /**
- * print_pointer - prints pointer
+ * print_pointer - prints a pointer
  * @val: value
  *
  * Return: int
  */
-
 int print_pointer(va_list val)
 {
 void *p;
@@ -14,13 +14,16 @@ long int a;
 int i, b;
 
 p = va_arg(val, void *);
+
 if (p == NULL)
 {
 for (i = 0; s[i] != '\0'; i++)
 _prints(s[i]);
 return (i);
 }
+
 a = (unsigned long int)p;
+
 _prints('0');
 _prints('x');
 b = print_convert_pointer(a);

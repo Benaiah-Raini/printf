d5f4b39d246d44bd73ca0719d748f9d82bef9850
Benaiah-Raini <benaiahraini@gmail.com>
2023-11-17 13:19:04 -0800

    bin error

--- print_bin.c
+++ print_bin.c
@@ -9,28 +9,28 @@
 
 int print_bin(va_list val)
 {
-	int flag = 0;
-	int count = 0;
-	int i, b;
-	unsigned int num = va_arg(val, unsigned int);
-	unsigned int p;
+int flag = 0;
+int count = 0;
+int i, b;
+unsigned int num = va_arg(val, unsigned int);
+unsigned int p;
 
-	for (i = 0; i < 32; i++)
-	{
-		p = ((1u << (31 - i)) & num);
-		if (p >> (31 - i))
-			flag = 1;
-		if (flag)
-		{
-			b = p >> (31 - i);
-			_prints(b + '0');
-			count++;
-		}
-	}
-	if (count == 0)
-	{
-		count++;
-		_prints('0');
-	}
-	return (count);
+for (i = 0; i < 32; i++)
+{
+p = ((1u << (31 - i)) & num);
+if (p >> (31 - i))
+flag = 1;
+if (flag)
+{
+b = p >> (31 - i);
+_prints(b + '0');
+count++;
+}
+}
+if (count == 0)
+{
+count++;
+_prints('0');
+}
+return (count);
 }

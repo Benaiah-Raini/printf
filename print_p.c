4eebeb913124aad14d464216aac97649d3249fb7
Benaiah-Raini <benaiahraini@gmail.com>
2023-11-17 13:45:45 -0800

    p-error

--- print_p.c
+++ print_p.c
@@ -8,21 +8,21 @@
 
 int print_pointer(va_list val)
 {
-	void *p;
-	char *s = "nil";
-	long int a;
-	int i, b;
+void *p;
+char *s = "nil";
+long int a;
+int i, b;
 
-	p = va_arg(val, void *);
-	if (p == NULL)
-	{
-		for (i = 0; s[i] != '\0'; i++)
-			_prints(s[i]);
-		return (i);
-	}
-	a = (unsigned long int)p;
-	_prints('0');
-	_prints('x');
-	b = print_convert_pointer(a);
-	return (b + 2);
+p = va_arg(val, void *);
+if (p == NULL)
+{
+for (i = 0; s[i] != '\0'; i++)
+_prints(s[i]);
+return (i);
+}
+a = (unsigned long int)p;
+_prints('0');
+_prints('x');
+b = print_convert_pointer(a);
+return (b + 2);
 }
